63dfbf54609c7875040c92fb7e3660c79cd7b544
doanhongha <doanhong.ha@monstar-lab.com>
2018-12-25 18:18:27 +0900

    feature/learn-methods-computed-watcher

---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="my-component">
+        <div>Message type: {{ message.type }}</div>
+        <div>Message text: {{ message.text }}</div>
+        <div><button @click="changeType">Change type</button></div>
+        <div><button @click="changeText">Change text</button></div>
+        <!-- <div>{{ sayHello(message) }}</div> -->
+        <!-- <div>{{ reverseMessage }}</div> -->
+        <!-- <div><button @click="sayHello">say hello</button></div>  -->
+        <!-- <div><button @click="sayHi">say hi</button></div> -->
+        <!-- <div><button @click="changeMessage">change message</button></div> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: {
+                    type: 'greeting',
+                    text: 'how are you?'
+                }
+            }
+        },
+        methods: {
+            sayHello(text) {
+                return text.toUpperCase()
+            },
+            sayHi() {
+                alert('say Hi')
+            },
+            changeMessage() {
+                this.message.text = 'this is new message'
+            },
+            changeType() {
+                this.message.type = 'this is new type'
+            },
+            changeText() {
+                this.message.text = 'this is new text'
+            }
+        },
+        computed: {
+            reverseMessage() {
+                return this.message.split('').reverse().join('')
+            },
+            getType() {
+                return this.message.type
+            },
+            getText() {
+                return this.message.text
+            }
+        },
+        watch: {
+            getType() {
+                console.log('type changed')
+            },
+            getText() {
+                console.log('text changed')
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .my-component {
+        color: red;
+    }
+</style>
